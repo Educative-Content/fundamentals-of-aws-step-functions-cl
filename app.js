a04f63c654757cf1cbc1782e71ce9da50cb74ac6
SyedArsalAhmad <114099407+SyedArsalAhmad@users.noreply.github.com>
2023-11-21 21:30:39 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const path = require('path');
+const axios = require('axios');
 
 const app = express();
 const port = 3000;
+const apiUrl = 'https://your-api-endpoint'; // Replace with your actual API endpoint
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: true }));
@@ -11,12 +13,26 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-app.post('/submit', (req, res) => {
+app.post('/submit', async (req, res) => {
     const inputValue = req.body.inputValue;
     const currentLength = inputValue.length;
     console.log(`Received input: ${inputValue}`);
     console.log(`Current length: ${currentLength}`);
-    res.send('Input received!');
+
+    try {
+        // Make a POST request to your API endpoint with the input value
+        const response = await axios.post(apiUrl, { inputValue });
+
+        // Log the API response
+        console.log('API Response:', response.data);
+
+        // Send a response to the client
+        res.send('Input received and sent to the API!');
+    } catch (error) {
+        // Handle errors
+        console.error('Error sending data to API:', error.message);
+        res.status(500).send('Error sending data to API');
+    }
 });
 
 app.listen(port, () => {
