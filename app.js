a04e79aa937de7cb077352788ffea94a63f2a433
SyedArsalAhmad <114099407+SyedArsalAhmad@users.noreply.github.com>
2023-11-22 16:31:03 +0500

    Update app.js

--- app.js
+++ app.js
@@ -4,15 +4,11 @@ const axios = require('axios');
 
 const app = express();
 const port = 3000;
-const apiUrl = 'https://<id>.execute-api.us-east-1.amazonaws.com/v1'; // Replace with your actual API endpoint
+apiUrl = 'https://<id>.execute-api.us-east-1.amazonaws.com/v1'; // Replace with your actual API endpoint
 apiUrl = apiUrl + "/?Token=<tasktoken>";
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: true }));
-
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
 
 app.post('/submit', async (req, res) => {
     const inputValue = req.body.inputValue;
